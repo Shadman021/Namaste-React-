313d5de31dff57044bba25ce923ec9385bfd7a2a
First-3-6-25-demo <shadman12278@gmail.com>
2025-06-06 14:48:06 +0530

    hello React

--- First_lect/script.js
+++ First_lect/script.js
@@ -1,4 +1,8 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "hello world";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML = "hello world";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+const heading = React.createElement("h2", {}, "Hello react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
