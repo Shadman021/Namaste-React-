355254eb06556c67deb3e6d07cf17baf6ce5f142
First-3-6-25-demo <shadman12278@gmail.com>
2025-06-06 15:26:02 +0530

    lecture complted

--- First_lect/script.js
+++ First_lect/script.js
@@ -3,6 +3,16 @@
 // const root = document.getElementById("root");
 // root.appendChild(heading);
 
-const heading = React.createElement("h2", {}, "Hello react");
+// const heading = React.createElement("h2", {}, "Hello react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},[  [React.createElement("h2", {id: "child1"},"This is child inside the parent element"), React.createElement("h3", {id: "child2"},"This is sibling")],
+
+  [React.createElement("h2", {id: "child3"},"This is child inside the parent element"), React.createElement("h3", {id: "child4"},"This is sibling")]]
+   
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
