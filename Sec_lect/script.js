a910063c51e6c675c8eb74c40643c9314d7b74ac
First-3-6-25-demo <shadman12278@gmail.com>
2025-06-10 16:05:15 +0530

    lecture 2 completed

--- Sec_lect/script.js
+++ Sec_lect/script.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 const parent = 
   React.createElement("div",{id: "parent", key: 1},[  [
   React.createElement("h2", {id: "child1",key: 2},"This is child inside the parent element"), 
-  React.createElement("h3", {id: "child2", key: 3},"This is sibling")],
+  React.createElement("h3", {id: "child2", key: 3},"completed")],
 
   [React.createElement("h2", {id: "child3", key: 4},"This is child inside the parent element"),
   React.createElement("h3", {id: "child4", key: 5},"This is sibling")]]
